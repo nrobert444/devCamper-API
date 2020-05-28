e7707e456c77a98400232940c899dd1399c2b519
Nicholas Roberto <njr444@gmail.com>
2020-05-28 10:40:50 -0700

    finalize devCamper API project

---
+++ routes/bootcamps.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const {
+  getBootcamp,
+  getBootcamps,
+  updateBootcamp,
+  deleteBootcamp,
+  createBootcamp,
+  getBootcampsInRadius,
+  bootcampPhotoUpload
+} = require('../controllers/bootcamps');
+
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
+
+//include other resource routers
+
+const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
+
+const router = express.Router();
+
+const { protect, authorize } = require('../middleware/auth');
+
+//re-route into other resource routers
+
+router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
+
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
+
+router
+  .route('/:id/photo')
+  .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
+
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(protect, authorize('publisher', 'admin'), createBootcamp);
+
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
+
+module.exports = router;
